7067f93d66f2f6590ccb013bee273b41ee43ed93
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-19 20:57:01 +0600

    initialize project setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+export default function App(){
+  const style={
+    fontSize:"30px"
+  }
+  return(
+    <Fragment>
+      <div>
+        <p style={style} >lorem</p>
+      </div>
+    </Fragment>
+  )
+}
