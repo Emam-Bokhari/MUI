c51ec1b232976f5be888b11bae436267111c06cc
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-19 21:44:12 +0600

    Typography * variant:body1,body2

--- src/components/TypographyComponent.jsx
+++ src/components/TypographyComponent.jsx
@@ -38,6 +38,15 @@ export default function TypographyComponent(){
       <Typography variant="subtitle2" >
         Lorem ipsum dolor sit amet.
       </Typography>
+
+      <Typography variant="body1" >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident adipisci officia eveniet nam tempora natus laboriosam. Dolorem earum exercitationem similique. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem id et odit natus reprehenderit dicta, voluptatem culpa excepturi ab possimus eos asperiores officia repellendus, in eligendi rerum nostrum placeat vero quasi expedita sit velit quia. Aspernatur nemo dicta odit qui exercitationem saepe? Quisquam quas perspiciatis, ad error praesentium vel magnam!
+      </Typography>
+
+<Typography variant="body2" sx={{marginTop:"20px",fontSize:"18px"}}>
+  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae aliquam suscipit nemo amet soluta aliquid sed eveniet eaque! Aperiam qui facilis ut autem fuga consequuntur animi voluptate voluptatibus! Pariatur libero saepe nisi! Necessitatibus tenetur debitis voluptatum laudantium nihil quas libero cupiditate ratione nulla veniam, delectus, earum fugit? Distinctio, ad nam!
+</Typography>
+
     </Fragment>
     )
 }
