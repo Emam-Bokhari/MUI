30c2a5b3a733c3af0d575625679478bb18bd1eab
Moshfiqur Rahman <studentemam@gmail.com>
2024-08-27 19:06:19 +0600

    button with icon

--- src/components/Practice/Day-02/ButtonComponent.jsx
+++ src/components/Practice/Day-02/ButtonComponent.jsx
@@ -1,5 +1,6 @@
 import { Button } from "@mui/material";
 import { Fragment } from "react";
+import SendIcon from "@mui/icons-material/Send"
 
 export default function ButtonComponent() {
   return (
@@ -9,33 +10,31 @@ export default function ButtonComponent() {
       <Button variant="outlined">Variant</Button>
       <Button variant="text">Variant</Button>
       {/* color */}
-      <Button color="primary"  variant="contained">
-        Color 
-      </Button>
-      <Button color="success" variant="contained" >
-        Color 
-      </Button>
-      <Button color="error" variant="contained" >
-        Color 
-      </Button>
-      <Button color="info" variant="contained" >
-        Color 
-      </Button>
-      <Button color="warning" variant="contained" >
+      <Button color="primary" variant="contained">
         Color
       </Button>
-      <Button color="inherit" variant="contained" >  
-        Color 
+      <Button color="success" variant="contained">
+        Color
       </Button>
-{/*       size */}
-      <Button size="small" >
-      Size
+      <Button color="error" variant="contained">
+        Color
       </Button>
-      <Button size="medium" >
-      Size
+      <Button color="info" variant="contained">
+        Color
       </Button>
-      <Button size="large" >
-      Size
+      <Button color="warning" variant="contained">
+        Color
+      </Button>
+      <Button color="inherit" variant="contained">
+        Color
+      </Button>
+      {/*  size */}
+      <Button size="small">Size</Button>
+      <Button size="medium">Size</Button>
+      <Button size="large">Size</Button>
+      {/* button with icon */}
+      <Button variant="outlined" endIcon={<SendIcon/>} >
+        Send 
       </Button>
     </Fragment>
   );
