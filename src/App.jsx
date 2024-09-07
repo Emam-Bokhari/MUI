a1bc4168f0b4c2ed2264f6c314fa31176d025c58
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-07 15:14:22 +0600

    feat(css): implemented MUI CssBaseline component for reset css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Fragment } from "react";
 import GridComponent from "./components/Layout/GridComponent";
+import { CssBaseline } from "@mui/material";
 
 export default function App() {
   return (
     <Fragment>
+      <CssBaseline/>
       <GridComponent />
     </Fragment>
   );
