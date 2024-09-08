b8b0ef5518100e9e1103cf0dbca5f0ae4f086285
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-08 15:48:12 +0600

    style(grid): implement responsive grid layout with spacing and flex-grow - add a responsive grid using MUI Grid component - apply column spacing and margin using sx prop

--- src/components/Layout/GridComponent.jsx
+++ src/components/Layout/GridComponent.jsx
@@ -28,20 +28,46 @@ export default function GridComponent() {
         </Grid>
       </Grid>
       {/* row and column spacing */}
-      <Grid sx={{mt:4}} container columnSpacing={2} rowSpacing={4}>
-      <Grid item xs={12} sm={6} md={3}>
-        <Paper style={{ padding: '16px', textAlign: 'center' }}>Responsive Box 1</Paper>
+      <Grid sx={{ mt: 4 }} container columnSpacing={2} rowSpacing={4}>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper style={{ padding: "16px", textAlign: "center" }}>
+            Responsive Box 1
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper style={{ padding: "16px", textAlign: "center" }}>
+            Responsive Box 2
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper style={{ padding: "16px", textAlign: "center" }}>
+            Responsive Box 3
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper style={{ padding: "16px", textAlign: "center" }}>
+            Responsive Box 4
+          </Paper>
+        </Grid>
       </Grid>
-      <Grid item xs={12} sm={6} md={3}>
-        <Paper style={{ padding: '16px', textAlign: 'center' }}>Responsive Box 2</Paper>
+      {/* auto */}
+      <Grid sx={{ mt: 4 }} columnSpacing={2} container>
+        <Grid item sx={{ flexGrow: 1 }}>
+          <Paper style={{ padding: "16px", textAlign: "center" }}>
+            Responsive Auto 1
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper style={{ padding: "16px", textAlign: "center" }}>
+            Responsive Auto 2
+          </Paper>
+        </Grid>
+        <Grid item sx={{ flexGrow: 1 }}>
+          <Paper style={{ padding: "16px", textAlign: "center" }}>
+            Responsive Auto 3
+          </Paper>
+        </Grid>
       </Grid>
-      <Grid item xs={12} sm={6} md={3}>
-        <Paper style={{ padding: '16px', textAlign: 'center' }}>Responsive Box 3</Paper>
-      </Grid>
-      <Grid item xs={12} sm={6} md={3}>
-        <Paper style={{ padding: '16px', textAlign: 'center' }}>Responsive Box 4</Paper>
-      </Grid>
-    </Grid>
     </Fragment>
   );
 }
