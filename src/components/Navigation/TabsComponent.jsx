72383699e4db6ff31bc8aee73a8f5744a64e31a1
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-29 06:43:15 +0600

    feat(tabs): add Tabs with text and indicator secondary color

--- src/components/Navigation/TabsComponent.jsx
+++ src/components/Navigation/TabsComponent.jsx
@@ -26,6 +26,21 @@ export default function TabsComponent() {
         <TabPanel value="3">Item Three</TabPanel>
         <TabPanel value="4">Item Four</TabPanel>
       </TabContext>
+      {/* text color, indicator color */}
+      <TabContext value={value} >
+        <Box sx={{ borderBottom: 2, borderColor: "divider" }}>
+          <TabList onChange={handleChange} textColor="secondary" indicatorColor="secondary">
+            <Tab label="All" value="1" />
+            <Tab label="Frontend" value="2" />
+            <Tab label="Backend" value="3" />
+            <Tab label="Others" value="4" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+        <TabPanel value="4">Item Four</TabPanel>
+      </TabContext>
     </Fragment>
   );
 }
