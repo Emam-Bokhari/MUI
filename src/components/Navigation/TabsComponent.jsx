c42e3a05d8e5d380b3a4ca20ac98ef45a1cda056
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-29 06:50:08 +0600

    feat(tabs): add variants for fullWidth,scrollable, centered and disable

--- src/components/Navigation/TabsComponent.jsx
+++ src/components/Navigation/TabsComponent.jsx
@@ -27,9 +27,93 @@ export default function TabsComponent() {
         <TabPanel value="4">Item Four</TabPanel>
       </TabContext>
       {/* text color, indicator color */}
-      <TabContext value={value} >
+      <TabContext value={value}>
         <Box sx={{ borderBottom: 2, borderColor: "divider" }}>
-          <TabList onChange={handleChange} textColor="secondary" indicatorColor="secondary">
+          <TabList
+            onChange={handleChange}
+            textColor="secondary"
+            indicatorColor="secondary"
+          >
+            <Tab label="All" value="1" />
+            <Tab label="Frontend" value="2" />
+            <Tab label="Backend" value="3" />
+            <Tab label="Others" value="4" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+        <TabPanel value="4">Item Four</TabPanel>
+      </TabContext>
+      {/* disable tab */}
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 2, borderColor: "divider" }}>
+          <TabList
+            onChange={handleChange}
+            textColor="secondary"
+            indicatorColor="secondary"
+          >
+            <Tab label="All" value="1" />
+            <Tab label="Frontend" value="2" />
+            <Tab label="Backend" value="3" disabled />
+            <Tab label="Others" value="4" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+        <TabPanel value="4">Item Four</TabPanel>
+      </TabContext>
+      {/* variant fullWidth,scrollable */}
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 2, borderColor: "divider" }}>
+          <TabList
+            variant="fullWidth"
+            onChange={handleChange}
+            textColor="secondary"
+            indicatorColor="secondary"
+          >
+            <Tab label="All" value="1" />
+            <Tab label="Frontend" value="2" />
+            <Tab label="Backend" value="3" />
+            <Tab label="Others" value="4" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+        <TabPanel value="4">Item Four</TabPanel>
+      </TabContext>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 2, borderColor: "divider" }}>
+          <TabList
+            variant="scrollable"
+            scrollButtons
+            allowScrollButtonsMobile
+            onChange={handleChange}
+            textColor="secondary"
+            indicatorColor="secondary"
+          >
+            <Tab label="All" value="1" />
+            <Tab label="Frontend" value="2" />
+            <Tab label="Backend" value="3" />
+            <Tab label="Others" value="4" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+        <TabPanel value="4">Item Four</TabPanel>
+      </TabContext>
+      {/* centered */}
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 2, borderColor: "divider" }}>
+          <TabList
+            centered
+            onChange={handleChange}
+            textColor="secondary"
+            indicatorColor="secondary"
+          >
             <Tab label="All" value="1" />
             <Tab label="Frontend" value="2" />
             <Tab label="Backend" value="3" />
