179dbbb90c9940e0cce8150d00dd89430b4b7abb
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-16 22:51:24 +0600

    chore: change theme primary color

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,7 +1,6 @@
 import { createTheme } from "@mui/material";
 import { pink, lime } from "@mui/material/colors";
 
-
 // Create the theme
 export const theme = createTheme({
   // start palette
@@ -10,10 +9,10 @@ export const theme = createTheme({
       main: "#FF014F",
     },
     success: {
-      main: "#F3971b",
+      main: "#F3971B",
     },
     textColor: {
-      primary: "orange",
+      primary: "indigo",
       secondary: "blue",
       bokhari: "green",
     },
@@ -25,42 +24,42 @@ export const theme = createTheme({
     },
   },
   // start typography
-  typography:{
-    fontFamily:"sans-serif",
-    fontSize:16,
-    fontWeightLight:300,
-    fontWeightRegular:400, 
-    fontWeightMedium:800,
-    fontWeightBold:900,
-    h1:{
-      fontFamily:"roboto",
-      fontWeight:700,
-      fontSize:"22px",
-      lineHeight:1,
-      letterSpacing:6
+  typography: {
+    fontFamily: "sans-serif",
+    fontSize: 16,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 800,
+    fontWeightBold: 900,
+    h1: {
+      fontFamily: "roboto",
+      fontWeight: 700,
+      fontSize: "22px",
+      lineHeight: 1,
+      letterSpacing: 6,
     }, // amon vabe h2,h3,h4,h5,h6,subtitle1,subtitle2,body1,body2 sob change kora jabe
-    body1:{
-      color:"white"
+    body1: {
+      color: "white",
     },
-    button:{
-      fontFamily:"arial",
-      fontWeight:500,
-      fontSize:18,
-      fontStyle:"italic",
-      lineHeight:2,
-      letterSpacing:0.3,
-      textTransform:"lowercase",
-    }
+    button: {
+      fontFamily: "arial",
+      fontWeight: 500,
+      fontSize: 18,
+      fontStyle: "italic",
+      lineHeight: 2,
+      letterSpacing: 0.3,
+      textTransform: "lowercase",
+    },
   },
   // start breakpoints
-  breakpoints:{
-    values:{
-      xs:0,
-      sm:576,
-      md:768,
-      lg:992,
-      xl:1200,
-      xxl:1400
-    }
-  }
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1200,
+      xxl: 1400,
+    },
+  },
 });
